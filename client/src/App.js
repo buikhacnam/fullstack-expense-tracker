d28bf77949a47d32254dbf1dd6b14b3514b526d8
Bui Nam <nambk@bravebits.vn>
2021-02-10 08:29:26 +0700

    change position

--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,8 @@ function App() {
         <div className='container'>
           <Balance />
           <IncomeExpenses />
-          <TransactionList />
           <AddTransaction />
+          <TransactionList />
         </div>
     </GlobalProvider>
   );
